a5cf8af2c4f0a4f681d292a1f4197bd6f2d28b5b
unknown <peter_piano@mail.ru>
2023-09-05 23:07:09 +0100

    my first commit

---
+++ 1.test.js
@@ -0,0 +1,13 @@
+test('status should be 200', async () => {
+    let actualStatus;
+    try {
+        // Make request
+        const response = await fetch("https://ebd371da-e3b8-4501-a5b0-f0fc9c0e22cb.serverhub.tripleten-services.com/api/v1/warehouses");
+        // Extract response code status
+        actualStatus = response.status;
+    } catch (error) {
+        console.error(error);
+    }
+    // Check code status
+    expect(actualStatus).toBe(200);
+}); 
